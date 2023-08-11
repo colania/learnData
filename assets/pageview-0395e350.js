20d43610e21c1e56252c91d7839d456f5ab8030b
colania <wweiooogo@gmail.com>
2023-08-11 09:34:47 +0000

    Deploying to gh-pages from @ colania/colania.github.io@5d8bce856c558c5ec48fa1f1c7e250b3cc7ea2f5 🚀

--- assets/pageview-0395e350.js
+++
@@ -1,1 +0,0 @@
-import{a5 as t}from"./app-231c8493.js";const n=()=>t({serverURL:"https://waline.newzone.top"});export{n as updatePageview};
---
+++ assets/pageview-c31ccebd.js
@@ -0,0 +1,1 @@
+import{a5 as t}from"./app-7a673887.js";const n=()=>t({serverURL:"https://waline.newzone.top"});export{n as updatePageview};
