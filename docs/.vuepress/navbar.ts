9a3d6089d37af28396ce234013f273c7068bdc07
edy <weiwei@wjinfo.cn>
2023-08-13 19:03:37 +0800

    test _post

--- docs/.vuepress/navbar.ts
+++ docs/.vuepress/navbar.ts
@@ -12,7 +12,7 @@ export default navbar([
       {
         text: "工作经验",
         prefix: "",
-        children: [],
+        children: ["_post/code-cr.md","_post/promotion-share.md"],
       },
       {
         text: "编码",
