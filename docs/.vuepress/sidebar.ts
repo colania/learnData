6882464d3ff6fcc765cc279cf251e45797255005
edy <weiwei@wjinfo.cn>
2023-09-07 13:45:41 +0800

    结构测试

--- docs/.vuepress/sidebar.ts
+++ docs/.vuepress/sidebar.ts
@@ -1,22 +1,61 @@
-// .vuepress/config.ts
-import { defineUserConfig } from "vuepress";
-import { hopeTheme } from "vuepress-theme-hope";
+import { sidebar } from "vuepress-theme-hope";
 
-export default defineUserConfig({
-  theme: hopeTheme({
-    sidebar: {
-      // "/DailyRoutine",
-
-      "/daily/": "structure",
-
-      "/bar/": "structure",
-
-      // fallback
-      "/": [
-        "" /* / */,
-        "contact" /* /contact.html */,
-        "about" /* /about.html */,
-      ],
-    },
-  }),
-});
+// 精选图标：https://theme-hope.vuejs.press/zh/guide/interface/icon.html#iconfont-%E7%B2%BE%E9%80%89%E5%9B%BE%E6%A0%87
+export default sidebar([
+  "/DailyRoutine",
+  // "/Fitness",
+  // 读书笔记架构更换到 docsify，不能使用相对链接
+  { text: "读书笔记",  link: "https://colania.github.io/reading/" },
+  {
+    text: "随心记",
+    prefix: "/daily/",
+    link: "",
+    collapsible: true,
+    children: "structure",
+  },
+  {
+    text: "程序人生",
+    prefix: "/code",
+    link: "",
+    collapsible: true,
+    children: "structure",
+  },
+  {
+    text: "应用手册",
+    prefix: "/apps/",
+    link: "",
+    collapsible: true,
+    children: [
+      "ChatGPT.md",
+      "AppNotes.md",
+      "Chrome.md",
+    ],
+  },
+  
+  {
+    text: "生活记录",
+    prefix: "/life/",
+    link: "",
+    collapsible: true,
+    children: "structure",
+  },
+  {
+    text: "开口说英语",
+    link: "",
+    collapsible: true,
+    children: [
+      {
+        text: "工作英语",
+        collapsible: true,
+        children: ["posts/meetingEnglish.md"],
+      },
+    ],
+  },
+  {
+    text: "金融小知识",
+    prefix: "/finance",
+    link: "",
+    collapsible: true,
+    children: "structure",
+  },
+]);
