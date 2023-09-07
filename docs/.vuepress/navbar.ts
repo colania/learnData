e0a6ab9dd8f2bccf131e80538a1abca2eb33c140
edy <weiwei@wjinfo.cn>
2023-09-07 16:47:41 +0800

    结构测试

--- docs/.vuepress/navbar.ts
+++ docs/.vuepress/navbar.ts
@@ -3,7 +3,7 @@ import { navbar } from "vuepress-theme-hope";
 // 精选图标：https://theme-hope.vuejs.press/zh/guide/interface/icon.html#iconfont-%E7%B2%BE%E9%80%89%E5%9B%BE%E6%A0%87
 export default navbar([
   { text: "博客", link: "/blog" },
-  { text: "精选", link: "/" },
+  { text: "目录", link: "/" },
   { text: "随心记", link: "/daily/summary" },
   {
     text: "Contact",
